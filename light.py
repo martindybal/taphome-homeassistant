6b24af8f64566297c852cbe38716459bb3091ef5
Martin Dybal <martin.dybal@riganti.cz>
2020-12-11 08:51:19 +0100

    Closes #18 refresh light state when value changed fails

--- light.py
+++ light.py
@@ -21,26 +21,19 @@ async def async_setup_platform(hass, config, async_add_entities, devices: list):
 
 
 async def async_create_light(tapHome: TapHome, device: TapHome.TapHomeDevice):
-    lightValues = (await tapHome.async_get_device_value(device.deviceId))["values"]
-    isLightOn = (
-        next(
-            lightValue
-            for lightValue in lightValues
-            if lightValue["valueTypeId"] == TapHome.ValueType.SwitchState.value
-        )["value"]
-        == TapHome.SwitchState.ON.value
-    )
-    return SimpleTapHomeLight(tapHome, device.deviceId, device.name, isLightOn)
+    light = SimpleTapHomeLight(tapHome, device.deviceId, device.name)
+    await light.async_refresh_state()
+    return light
 
 
 class SimpleTapHomeLight(LightEntity):
     """Representation of an Light without brightness or color setting."""
 
-    def __init__(self, tapHome: TapHome, deviceId: int, name: str, is_on: bool):
+    def __init__(self, tapHome: TapHome, deviceId: int, name: str):
         self._tapHome = tapHome
         self._name = name
         self._deviceId = deviceId
-        self._is_on = is_on
+        self._is_on: bool = None
 
     @property
     def name(self):
@@ -55,9 +48,30 @@ class SimpleTapHomeLight(LightEntity):
     async def async_turn_on(self, **kwargs):
         """Turn device on."""
         result = await self._tapHome.async_turn_on_light(self._deviceId)
-        self._is_on = result == TapHome.ValueChangeResult.CHANGED
+
+        if result == TapHome.ValueChangeResult.FAILED:
+            await self.async_refresh_state()
+        else:
+            self._is_on = True
 
     async def async_turn_off(self):
         """Turn device off."""
         result = await self._tapHome.async_turn_off_light(self._deviceId)
-        self._is_on = not result == TapHome.ValueChangeResult.CHANGED
+
+        if result == TapHome.ValueChangeResult.FAILED:
+            await self.async_refresh_state()
+        else:
+            self._is_on = False
+
+    async def async_refresh_state(self):
+        lightValues = (await self._tapHome.async_get_device_value(self.deviceId))[
+            "values"
+        ]
+        self._is_on = (
+            next(
+                lightValue
+                for lightValue in lightValues
+                if lightValue["valueTypeId"] == TapHome.ValueType.SwitchState.value
+            )["value"]
+            == TapHome.SwitchState.ON.value
+        )
