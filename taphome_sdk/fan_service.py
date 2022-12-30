a092b3b33e3ad96e6f06d1d0069ff44e411034b9
Martin Dybal <martin@dybal.it>
2022-12-30 20:57:58 +0100

    Add fan

---
+++ taphome_sdk/fan_service.py
@@ -0,0 +1,58 @@
+from .device import Device
+from .taphome_api_service import TapHomeApiService
+from .value_type import ValueType
+from .taphome_device_state import TapHomeState
+from .switch_states import SwitchStates
+
+
+class FanState(TapHomeState):
+    def __init__(
+        self,
+        fan_values: dict,
+    ):
+        super().__init__(fan_values)
+        self.switch_state = SwitchStates(self.get_device_value(ValueType.SwitchState))
+        self.percentage = self.get_device_value(ValueType.AnalogOutputValue)
+
+
+class FanService:
+    def __init__(self, tapHomeApiService: TapHomeApiService):
+        self.tapHomeApiService = tapHomeApiService
+
+    async def async_get_state(self, device: Device) -> FanState:
+        fan_values = await self.tapHomeApiService.async_get_device_values(
+            device.deviceId
+        )
+
+        return FanState(fan_values)
+
+    def async_turn_on(self, device: Device) -> None:
+        values = [
+            self.tapHomeApiService.create_device_value(
+                ValueType.SwitchState, SwitchStates.ON.value
+            )
+        ]
+        return self.tapHomeApiService.async_set_device_values(device.id, values)
+
+    def async_turn_off(self, device: Device) -> None:
+        values = [
+            self.tapHomeApiService.create_device_value(
+                ValueType.SwitchState, SwitchStates.OFF.value
+            )
+        ]
+        return self.tapHomeApiService.async_set_device_values(device.id, values)
+
+    def async_set_percentage(self, device: Device, percentage=None) -> None:
+        if percentage == 0:
+            return self.async_turn_off(device)
+        else:
+            values = [
+                self.tapHomeApiService.create_device_value(
+                    ValueType.SwitchState, SwitchStates.ON.value
+                ),
+                self.tapHomeApiService.create_device_value(
+                    ValueType.AnalogOutputDesiredValue, percentage
+                ),
+            ]
+
+            return self.tapHomeApiService.async_set_device_values(device.id, values)
