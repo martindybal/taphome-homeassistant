ca0a2d13a8703d5a921a8171256344295b361e07
Martin Dybal <martin@dybal.it>
2025-07-21 22:53:01 +0200

    Simplify object

--- taphome_core_config_entry.py
+++ taphome_core_config_entry.py
@@ -7,11 +7,6 @@ from dataclasses import dataclass
 class TapHomeCoreConfigEntry:
     """Holds configuration options for a TapHome core instance."""
 
-    core_id: str
+    id: str
     use_description_as_entity_id: bool
     use_description_as_name: bool
-
-    @property
-    def id(self) -> str:
-        """Return the identifier of the TapHome core."""
-        return self.core_id
